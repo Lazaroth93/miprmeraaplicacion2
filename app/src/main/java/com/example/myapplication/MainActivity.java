42e14b3664d4b703b7439773f25dbb9a01f17a8a
Andres <andres.lazaro.nu@gmail.com>
2021-07-29 15:05:39 +0200

    subiendo cambios.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Comprobando que funciona nuestra control de versiones", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
